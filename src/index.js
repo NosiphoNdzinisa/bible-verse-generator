b3e5b6c5eda88986c3c32959afb5cad64107b3ee
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-08-09 19:42:38 +0200

    Formatted previous work and started afresh

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-function displayVerse(response) {
-    console.log("verse generated");
-    new Typewriter("#verse", {
-        strings: response.data.answer,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-    });
-    }
+function generatePoem(event) {
+  event.preventDefault();
 
-
-function generateVerse(event) {
-    event.preventDefault();
-
-    let instructionsInput = document.querySelector("user-instructions");
-    let apiKey = "46eb1514fecb5a730ct10b7d5o64c5f7";
-    let prompt = `User instructions: Generate a bible verse about ${instructionsInput.value}`;
-    let context =
-    "You are a Bible verse expert who provides accurate scripture with references, and explains it in a short, uplifting sentence.Make sure to follow user instructions.";
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-    console.log("Generating verse");
-    console.log(`prompt: ${prompt}`);
-    console.log(`context: ${context}`);
-    
-    axios.get(apiUrl).then(displayVerse)
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
-
-let verseFormElement = document.querySelector("verseGeneratorForm");
-verseFormElement.addEventListener("submit", generateVerse);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
