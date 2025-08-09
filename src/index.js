c79af6ac3f0f18cbbebea7a6e3f13eee7e2a585d
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-08-09 17:59:09 +0200

    Added the API intergration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+function displayVerse(response) {
+    console.log("verse generated");
+    new Typewriter("#verse", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+    }
+
+
 function generateVerse(event) {
     event.preventDefault();
 
+    let instructionsInput = document.querySelector("user-instructions");
+    let apiKey = "46eb1514fecb5a730ct10b7d5o64c5f7";
+    let prompt = `User instructions: Generate a bible verse about ${instructionsInput.value}`;
+    let context =
+    "You are a Bible verse expert who provides accurate scripture with references, and explains it in a short, uplifting sentence.Make sure to follow user instructions.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-new Typewriter("#verse", {
-    strings: ["Hello", "World"],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-});
-verseElement.innerHTML = ""
+    console.log("Generating verse");
+    console.log(`prompt: ${prompt}`);
+    console.log(`context: ${context}`);
+    
+    axios.get(apiUrl).then(displayVerse)
 }
 
 
