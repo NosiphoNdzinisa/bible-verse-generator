f78b0c6106ec1b50651096f21b09a36b9ab5e44e
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-08-13 18:51:53 +0200

    Updated title of App

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function displayPoem(response) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "46eb1514fecb5a730ct10b7d5o64c5f7";
     let context =
-      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'The Altar-within AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let prompt = `User instructions: Generate a bible verse inspired poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
